422c398a8629168d0d3def781ab68d4adf810593
Joseph H <jhomrocky@gmail.com>
2018-03-07 20:28:46 -0500

    Find a Location box added, CSS done, finished.

--- Scripts/JavaScript.js
+++ Scripts/JavaScript.js
@@ -1,1 +1,50 @@
-﻿
+﻿//Cities taken from http://worldpopulationreview.com/states/michigan-population/cities/
+//In this mockup world, I decided that OCP now operates out of the 10 biggest cities in Michigan
+//And the 11th might be an easter egg :)
+
+const cities = [{
+    value: "https://www.google.com/maps/place/Detroit,+MI/",
+    label: "Detroit"
+},
+{
+    value: "https://www.google.com/maps/place/Grand+Rapids,+MI/",
+    label: "Grand Rapids"
+},
+{
+    value: "https://www.google.com/maps/place/Warren,+MI/",
+    label: "Warren"
+},
+{
+    value: "https://www.google.com/maps/place/Sterling+Heights,+MI/",
+    label: "Sterling Heights, MI"
+},
+{
+    value: "https://www.google.com/maps/place/Ann+Arbor,+MI",
+    label: "Ann Arbor, MI"
+},
+{
+    value: "https://www.google.com/maps/place/Lansing,+MI/",
+    label: "Lansing, MI"
+},
+{
+    value: "https://www.google.com/maps/place/Flint,+MI/",
+    label: "Flint, MI"
+},
+{
+    value: "https://www.google.com/maps/place/Dearborn,+MI/",
+    label: "Dearborn, MI"
+},
+{
+    value: "https://www.google.com/maps/place/Livonia,+MI/",
+    label: "Livonia, MI"
+},
+{
+    value: "https://www.google.com/maps/place/Troy,+MI/",
+    label: "Troy, MI"
+},
+{
+    value: "https://www.google.com/maps/place/9233+Church+Rd,+Dallas,+TX+75231/@32.8859871,-96.7317616,3a,75y,3.43h,90t/data=!3m6!1e1!3m4!1sxqgA5YIoHy9jftPXU06-VQ!2e0!7i13312!8i6656!4m5!3m4!1s0x864ea01273c6d985:0xea3d746336ee0c48!8m2!3d32.8861956!4d-96.7317427",
+    label: "Murphy's Secret Hideout"
+    }
+];
+
